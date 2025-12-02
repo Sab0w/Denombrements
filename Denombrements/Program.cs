0d604f7909d9949c8b81616b464a3761f6c02f02
Corentin POISSON <corentin.gaming@hotmail.fr>
2025-12-02 03:39:47 +0100

    Modifications du code et ajouts du module qui gère les chaînes de caractères . Bloque la possibilité à l'utilisateur de choisir un chiffre plus grand que 3 dans le menu de selection

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -18,41 +18,62 @@ namespace Denombrements
                 Console.WriteLine("Combinaison ...................... 3");
                 Console.WriteLine("Quitter .......................... 0");
                 Console.Write("Choix :                            ");
-                c = int.Parse(Console.ReadLine());
-
-                if (c == 0) { Environment.Exit(0); }
+                c = LireEntier();
+                {
+                    if ( c < 0 || c > 3)
+                    {
+                        Console.WriteLine("Choisissez un nombre entre 0 et 3");
+                        continue; // retour au menu
+                    }
+                }
+                if (c == 0) 
+                { 
+                    Environment.Exit(0);
+                    
+                }
 
                 if (c == 1)
                 {
-                    Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                    int n = int.Parse(Console.ReadLine()); // saisir le nombre
-                                                           // calcul de r
+                    // le nombre d'éléments à gérer
+                    Console.Write("nombre total d'éléments à gérer = ");
+                    // saisir le nombre
+                    int n = LireEntier(); 
+                    // calcul de r
                     long r = 1;
                     for (int k = 1; k <= n; k++)
                         r *= k;
+                    // affichage du résultat
                     Console.WriteLine(n + "! = " + r);
                 }
                 else
                 {
                     if (c == 2)
                     {
-                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                        int t = int.Parse(Console.ReadLine()); // saisir le nombre
-                        Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
-                        int n = int.Parse(Console.ReadLine()); // saisir le nombre
+                        // le nombre d'éléments à gérer
+                        Console.Write("nombre total d'éléments à gérer = ");
+                        // saisir le nombre
+                        int t = int.Parse(Console.ReadLine());
+                        // le sous ensemble
+                        Console.Write("nombre d'éléments dans le sous ensemble = "); 
+                        // saisir le nombre
+                        int n = int.Parse(Console.ReadLine()); 
                         // calcul de r
                         long r = 1;
                         for (int k = (t - n + 1); k <= t; k++)
                             r *= k;
-                        //Console.WriteLine("résultat = " + (r1 / r2));
+                        // affichage du résultat
                         Console.WriteLine("A(" + t + "/" + n + ") = " + r);
                     }
                     else
                     {
-                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
-                        int t = int.Parse(Console.ReadLine()); // saisir le nombre
-                        Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
-                        int n = int.Parse(Console.ReadLine()); // saisir le nombre
+                        // le nombre d'éléments à gérer
+                        Console.Write("nombre total d'éléments à gérer = "); 
+                        // saisir le nombre
+                        int t = int.Parse(Console.ReadLine());
+                        // le sous ensemble
+                        Console.Write("nombre d'éléments dans le sous ensemble = "); 
+                        // saisir le nombre
+                        int n = int.Parse(Console.ReadLine()); 
                         // calcul de r1
                         long r1 = 1;
                         for (int k = (t - n + 1); k <= t; k++)
@@ -61,13 +82,27 @@ namespace Denombrements
                         long r2 = 1;
                         for (int k = 1; k <= n; k++)
                             r2 *= k;
-                        // calcul de r3
-                        //Console.WriteLine("résultat = " + (r1 / r2));
+                        // affichage du résultat
                         Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2));
                     }
                 }
             }
             Console.ReadLine();
+        }
+        ///<summary>
+        ///Lecture d'erreurs et correction
+        ///</summary>
+        private static int LireEntier()
+        {
+            {
+
+                int reponse;
+                while (!int.TryParse(Console.ReadLine(), out reponse))
+                {
+                    Console.WriteLine("Erreur de saisie , recommencez : ");
+                }
+                return reponse;
+            }
         }
     }
 }
